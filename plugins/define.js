c69043e80dc2a44381ad546e862f0f267d4e3791
Nick Van Wiggeren <nick@facepwn.com>
2012-12-24 18:00:27 -0800

    Updated define logic

--- plugins/define.js
+++ plugins/define.js
@@ -5,11 +5,13 @@ function process(nick, to, cmd, topass) {
 	var url = "http://api.wordnik.com/v4/word.json/"+topass+"/definitions?includeRelated=false&api_key="+apikey+"&includeTags=false&limit=1&useCanonical=true";
 	fetch(url, function (error, meta, body){
 		var data = JSON.parse(body.toString());
-		if (data == "") {
+		if (!data.hasOwnProperty(0)) {
 			bot.say(to, "Word doesn't exist");
 		}
 		else if(data[0].hasOwnProperty('word')) {
 			bot.say(to, data[0].word+": "+ data[0].text);
+		} else {
+			bot.say(to, "Word doesn't exist");
 		}
 	});
 }
