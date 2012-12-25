3b5f2dc92b98ea609bddbdb84e18f8c7fe679ea5
Nick Van Wiggeren <nick@facepwn.com>
2012-12-24 19:28:10 -0800

    error checking for dictionary

--- plugins/define.js
+++ plugins/define.js
@@ -1,17 +1,26 @@
 listen = /define/i;
 var apikey = bot.api.wordnik;
+
 function process(nick, to, cmd, topass) {
-	var fetch = require('fetch').fetchUrl;
-	var url = "http://api.wordnik.com/v4/word.json/"+topass+"/definitions?includeRelated=false&api_key="+apikey+"&includeTags=false&limit=1&useCanonical=true";
-	fetch(url, function (error, meta, body){
-		var data = JSON.parse(body.toString());
-		if (!data.hasOwnProperty(0)) {
-			bot.say(to, "Word doesn't exist");
-		}
-		else if(data[0].hasOwnProperty('word')) {
-			bot.say(to, data[0].word+": "+ data[0].text);
-		} else {
-			bot.say(to, "Word doesn't exist");
-		}
-	});
+    var fetch = require('fetch').fetchUrl;
+    var onlyLetters = /^[a-zA-Z]*$/.test(topass);
+    if (!onlyLetters) {
+        bot.say(to, "Invalid Word");
+        return;
+    }
+    var url = "http://api.wordnik.com/v4/word.json/" + topass + "/definitions?includeRelated=false&api_key=" + apikey + "&includeTags=false&limit=1&useCanonical=true";
+    fetch(url, function (error, meta, body) {
+        try {
+            var data = JSON.parse(body.toString());
+            if (!data.hasOwnProperty(0)) {
+                bot.say(to, "Word doesn't exist");
+            } else if (data[0].hasOwnProperty('word')) {
+                bot.say(to, data[0].word + ": " + data[0].text);
+            } else {
+                bot.say(to, "Word doesn't exist");
+            }
+        } catch (err) {
+            bot.say(to, err);
+        }
+    });
 }
