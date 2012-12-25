4d66b3e43e0b311e45db61fee0532e2725381236
Nick Van Wiggeren <nick@facepwn.com>
2012-12-24 17:39:21 -0800

    Added definition script

---
+++ plugins/define.js
@@ -0,0 +1,15 @@
+listen = /define/i;
+var apikey = bot.api.wordnik;
+function process(nick, to, cmd, topass) {
+	var fetch = require('fetch').fetchUrl;
+	var url = "http://api.wordnik.com/v4/word.json/"+topass+"/definitions?includeRelated=false&api_key="+apikey+"&includeTags=false&limit=1&useCanonical=true";
+	fetch(url, function (error, meta, body){
+		var data = JSON.parse(body.toString());
+		console.log(data[0].word);
+		if (data[0].hasOwnProperty('word')) {
+			bot.say(to, data[0].word+": "+ data[0].text);
+		} else {
+			bot.say(to, "Word doesn't exist");
+		}
+	});
+}
