1594acc369b04f71501e485a279889bd192c9902
Nick Van Wiggeren <nick@facepwn.com>
2012-12-27 16:49:45 -0800

    Added 8ball

---
+++ plugins/8ball.js
@@ -0,0 +1,38 @@
+var listen = /8ball/i;
+
+function process(nick, to, cmd, topass) {
+        var outcomes = [
+                        "It is certain",
+                        "It is decidedly so",
+                        "Without a doubt",
+                        "Yes – definitely",
+                        "You may rely on it",
+                        "As I see it, yes",
+                        "Most likely",
+                        "Outlook good",
+                        "Yes",
+                        "Signs point to yes",
+                        "Reply hazy, try again",
+                        "Ask again later",
+                        "Better not tell you now",
+                        "Cannot predict now",
+                        "Concentrate and ask again",
+                        "Don't count on it",
+                        "My reply is no",
+                        "My sources say no",
+                        "Outlook not so good",
+                        "Very doubtful"];
+
+
+        if (topass.lengh < 2) {
+                bot.say(to,'[8ball] Do you have a quesiton?');
+
+        } else {
+            	bot.say(to,'[8ball] ' + outcomes[ Math.floor(Math.random() * outcomes.length)] );
+        }
+
+}
+
+
+
+
